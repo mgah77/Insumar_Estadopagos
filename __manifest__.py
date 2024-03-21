b6d6db5f0220decf5b4cfb5acd7d652012ddfaac
mgah77 <mauricio.gah@gmail.com>
2024-03-21 20:44:44 -0300

    1

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{ 'name': 'Insumar_taller',
+'summary': "OT y certificaciones",
+'author': "Mauricio Gah",
+'license': "AGPL-3",
+'application': "True",
+'version': "2.0",
+'data': ['security/groups.xml',
+         'security/ir.model.access.csv',
+         'views/menu.xml',
+         'views/maestro.xml',
+         'views/product_template.xml',
+         'views/orden_trabajo.xml',
+         'views/planificacion.xml',
+         'views/maniobras.xml'
+],
+'depends': ['base' , 'contacts' , 'stock' , 'product', 'parches_insumar']
+}
