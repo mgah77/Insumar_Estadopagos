29cbb015d1e4a4e9d720afeab20339d5d9bd6133
mgah77 <mauricio.gah@gmail.com>
2024-03-27 10:45:28 -0300

    Update estado_wizard.py

--- wizard/estado_wizard.py
+++ wizard/estado_wizard.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 from datetime import date
 
 class EstadoWizard(models.TransientModel):
-    _name = 'Estadopago.wizard'
+    _name = 'Estado_wizard'
     _description = 'Estado de pago de clientes'
 
     cliente = fields.Many2one('res.partner', string='Cliente',domain=[('is_company','=',True)])
