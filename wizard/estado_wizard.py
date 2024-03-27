d10871af85b0c3e08706fdfd0680f8d53a22b79f
mgah77 <mauricio.gah@gmail.com>
2024-03-27 10:50:06 -0300

    we

--- wizard/estado_wizard.py
+++ wizard/estado_wizard.py
@@ -2,8 +2,8 @@ from odoo import models, fields, api
 from datetime import date
 
 class EstadoWizard(models.TransientModel):
-    _name = 'Estado_wizard'
-    _description = 'Estado de pago de clientes'
+    _name = "wizard.estadopago"
+    _description = "Estado de pago de clientes"
 
     cliente = fields.Many2one('res.partner', string='Cliente',domain=[('is_company','=',True)])
     fac_vencida = fields.Integer(string = 'Facturas Vencidas')
